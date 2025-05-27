4236feff3e9b358ecc1962ef44601d86714e5870
robertoleite.rl@gmail.com <robertoleite.rl@gmail.com>
2025-05-27 20:56:24 -0300

    Ajuste ex011

--- ex011/cap.php
+++ ex011/cap.php
@@ -12,12 +12,16 @@
     <main>
         <h1>Resultado Final</h1>
         <?php $valor = (float)$_GET["numbertxt"];
+       
+        $real = numfmt_create('pt_BR', NumberFormatter::CURRENCY);
+        $dolar = numfmt_create('en_US', NumberFormatter::CURRENCY);
         $valordolar = floatval(5.69);
         $conversao = floatval($valor / $valordolar);
+       
         ?>
         <div>
-            <p><?php echo "Seus R$ $valor equivalem a <strong> US$ $conversao </strong>" ?></p>
-            <p><?php echo "<strong>Cotação fixa de: $valordolar </strong> informada direto no código" ?></p>
+            <p><?php echo "Seus ". numfmt_format_currency($real, $valor, "BRL" ). " equivalem a <strong> ". numfmt_format_currency($dolar, $conversao, "USD" ) ." </strong>" ?></p>
+            <p><?php echo "<strong>Cotação fixa de: ".numfmt_format_currency($dolar, $valordolar, "USD" )." </strong> informada direto no código" ?></p>
             
         </div>
         <div>
